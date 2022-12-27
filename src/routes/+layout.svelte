9218e7359f8688ded7e1c65f5b4d65d5b7ebaf3e
Sharn-Konet Reitsma <sharnkonet.reitsma@gmail.com>
2022-12-28 10:33:56 +1300

    Factor global css into its own file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import Navbar from './Navbar.svelte'
+    import './global.css'
 </script>
 
 <svelte:head>
@@ -10,38 +11,3 @@
     <Navbar/>
     <slot/>
 </div>
-
-<style>
-
-    :global(:root) {
-        --light-mode-text-color: 0, 132, 245;
-        --light-mode-background-color: 242, 238, 226;
-
-        --dark-mode-text-color: 228, 230, 235;
-        --dark-mode-background-color: 24, 25, 26;
-    }
-
-   :global(*) {
-        font-family: "Alata", sans-serif;
-        font-size: 1rem;
-    }
-
-    :global(body) {
-        background-color: rgb(var(--light-mode-background-color));
-        transition: background-color 0.3s;
-        color: rgb(var(--light-mode-text-color));
-        border-color: rgb(var(--light-mode-text-color));
-        transition: color 0.1s;
-
-        line-height: 1.5;
-        text-align: left;
-    }
-
-    :global(body.dark-mode) {
-        background-color: rgb(var(--dark-mode-background-color));
-        color: rgb(var(--dark-mode-text-color));
-    }
-
-    #background {}
-</style>
-
