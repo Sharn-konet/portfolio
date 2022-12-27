cb7ce8e271cbd3ccf710fbaa6374a19e80bfebc8
Sharn-Konet Reitsma <sharnkonet.reitsma@gmail.com>
2022-12-27 20:10:14 +1300

    Add experience component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,11 @@
 	import Experience from "./Experience.svelte";
     import Education from "./Education.svelte";
     import Hero from "./Hero.svelte"
+    import Introduction from "./Introduction.svelte"
 </script>
 
 <Hero/>
+<Experience/>
 
 <style>
 
--- src/routes/Experience.svelte
+++ src/routes/Experience.svelte
@@ -1,25 +1,87 @@
+<script>
+    import {Timeline} from '@svelteuidev/core'
+</script>
+
+<div id = 'experience'>
 <h1>Experience</h1>
-<div>
-    <h2>Data Scientist Graduate at <strong>Xero</strong></h2>
-    <p>
-        In the graduate program at Xero I rotated through multiple disciplines.
+<Timeline active={4} lineWidth={4} override={{color: '#ffffff'}}>
+    <Timeline.Item>
+        <div class = title>
+            <h3>Data Scientist Graduate at Xero</h3>
+            <h4>Feb 2022 - present</h4>
+        </div>
+        <p>In the graduate program at Xero I rotated through multiple disciplines.</p>
 
-        As a Data Applications Engineer I was learning to serve production-ready Machine Learning systems. I also got to work on develop internal tooling for orchestrating complex data pipelines.
+        <ul>
+        <li>As a Data Applications Engineer I was learning to serve production-ready Machine Learning systems. I also got to work on develop internal tooling for orchestrating complex data pipelines.</li>
 
-        As an Applied Scientist I was engaged in developing better predictions for Bank Reconciliation. This gave me the opportunity to experience how experiments are conducted in large organisations improving existing products.
+        <li>As an Applied Scientist I was engaged in developing better predictions for Bank Reconciliation. This gave me the opportunity to experience how experiments are conducted in large organisations improving existing products.</li>
 
-        As a Data Enrichment Engineer, I implemented business logic using the Data Vault 2.0 modelling standard. I also worked on making the build process more efficient for existing models."""
-    </p>
+        <li>As a Data Enrichment Engineer, I implemented business logic using the Data Vault 2.0 modelling standard. I also worked on making the build process more efficient for existing models.</li>
+        </ul>
+    </Timeline.Item>
 
-    <h2>AI Software Developer at Spark 64</h2>
-    <p>
-        Through my internship at ElementX, previously Spark64, I gained the opportunity to branch out from analytics and develop AI solutions for a wide range of areas. During this internship I took on the responsibility of developing computer vision solutions for optical character recognition, object recognition and audio event detection.
-    </p>
+    <Timeline.Item>
+        <div class = title>
+            <h3>AI Software Developer at Spark 64</h3>
+            <h4>Nov 2020 - Feb 2021</h4>
+        </div>
+        <p>
+            Through my internship at ElementX, previously Spark64, I gained the opportunity to branch out from analytics and develop AI solutions for a wide range of areas. During this internship I took on the responsibility of developing computer vision solutions for optical character recognition, object recognition and audio event detection.
+        </p>
+    </Timeline.Item>
 
-    <h2>Research Assistant</h2>
+    <Timeline.Item>
+        <div class = title>
+            <h3>Research Assistant at the University of Auckland</h3>
+            <h4>Sep 2020 - Jan 2022</h4>
+        </div>
+    </Timeline.Item>
 
-    <h2>Data Scientist Intern at <strong>Harmonic Analytics</strong></h2>
-    <p>
-        My internship at Harmonic was my first exposure to analytics and optimisation in a professional environment. I primarily worked as a contractor for Watercare which involved analytics of wastewater overflow, and application of computer vision to asset management.
-    </p>
+    <Timeline.Item>
+        <div class = title>
+            <h3>Data Scientist Intern at Harmonic Analytics</h3>
+            <h4>Dec 2019 - Mar 2020</h4>
+        </div>
+        <p>
+            My internship at Harmonic was my first exposure to analytics and optimisation in a professional environment. I primarily worked as a contractor for Watercare which involved analytics of wastewater overflow, and application of computer vision to asset management.
+        </p>
+    </Timeline.Item>
+</Timeline>
 </div>
+
+<style>
+    #experience {
+        margin: 10% 25%
+    }
+
+    h1 {
+        text-align: center;
+        font-size: 3em
+    }
+
+    h3, h4 {
+        margin-block-start: 0px;
+    }
+
+    .title {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    :global(body) p,
+    :global(body) li,
+    :global(body) h3,
+    :global(body) h4 {
+        color: var(--light-mode-text-color)
+    }
+
+    :global(body.dark-mode) p,
+    :global(body.dark-mode) li,
+    :global(body.dark-mode) h3,
+    :global(body.dark-mode) h4 {
+        color: #ffffff
+    }
+</style>
