93a7cc41c4beb458b26580314bf21d46cd978d9e
Sharn-Konet Reitsma <sharnkonet.reitsma@gmail.com>
2022-12-27 20:10:16 +1300

    Add new components to landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,13 @@
     import Education from "./Education.svelte";
     import Hero from "./Hero.svelte"
     import Introduction from "./Introduction.svelte"
+    import Projects from "./Projects.svelte"
 </script>
 
 <Hero/>
+
 <Experience/>
 
-<style>
+<Education/>
 
-</style>
+<Projects/>
