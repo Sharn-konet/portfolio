815c535e0c4b1cd4b5ac0599fc0e8cbfffe619c1
Sharn-Konet Reitsma <sharnkonet.reitsma@gmail.com>
2022-12-28 10:35:45 +1300

    Remove styling from old darkmode button

--- src/routes/Darkmode.svelte
+++ src/routes/Darkmode.svelte
@@ -20,19 +20,3 @@
 </script>
 
 <Switch on:change={() => {isDark = !isDark}} checked={isDark}/>
-
-<style>
-    button {
-        background-color: #f76027;
-        color: white;
-        border: none;
-        border-radius: 4px;
-        padding: 0.5rem;
-        text-transform: uppercase;
-
-    }
-    :global(body.dark-mode) button {
-        background-color: #0084f6;
-        color: white;
-    }
-</style>
