523a30704de91bd22608245693b22fe37e01a772
Sharn-Konet Reitsma <sharnkonet.reitsma@gmail.com>
2023-09-02 23:17:11 +1200

    DRAGGING!!!!!!!

--- src/routes/sections/Network.svelte
+++ src/routes/sections/Network.svelte
@@ -19,7 +19,7 @@ const svg = d3.select("#my_dataviz")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)      
   .attr("viewBox", [0, 0, width, height])
-  .attr("style", "max-width: 100%; height: auto;")
+  .attr("style", "max-width: 100%; height: auto; margin-left: 20%; border-color: white; border-style: solid; margin-top: 15%;")
 .append("g")
   .attr("transform",
         `translate(${margin.left}, ${margin.top})`);
@@ -36,7 +36,7 @@ const svg = d3.select("#my_dataviz")
     .selectAll("circle")
     .data(data.nodes)
     .join("circle")
-      .attr("r", 20)
+      .attr("r", 10)
       .style("fill",  "#0084f5")
 
   // Let's list the force we wanna apply on the network
@@ -45,9 +45,15 @@ const svg = d3.select("#my_dataviz")
             .id(function(d) { return d.id; })                     // This provide  the id of a node
             .links(data.links)                                    // and this the list of links
       )
-      .force("charge", d3.forceManyBody().strength(-100))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-      .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
+      .force("charge", d3.forceManyBody())         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+      .force("center", d3.forceCenter(width / 2, height / 2).strength(1))     // This force attracts nodes to the center of the svg area
       .on("tick", ticked);
+
+  // Add a drag behavior.
+  node.call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
 
   // This function is run at each iteration of the force algorithm, updating the nodes position.
   function ticked() {
@@ -58,8 +64,29 @@ const svg = d3.select("#my_dataviz")
         .attr("y2", function(d) { return d.target.y; });
 
     node
-         .attr("cx", function (d) { return d.x+6; })
-         .attr("cy", function(d) { return d.y-6; });
+         .attr("cx", function (d) { return d.x; })
+         .attr("cy", function(d) { return d.y; });
+  }
+
+  // Reheat the simulation when drag starts, and fix the subject position.
+  function dragstarted(event) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    event.subject.fx = event.subject.x;
+    event.subject.fy = event.subject.y;
+  }
+
+  // Update the subject (dragged node) position during drag.
+  function dragged(event) {
+    event.subject.fx = event.x;
+    event.subject.fy = event.y;
+  }
+
+  // Restore the target alpha so the simulation cools after dragging ends.
+  // Unfix the subject position now that it’s no longer being dragged.
+  function dragended(event) {
+    if (!event.active) simulation.alphaTarget(0);
+    event.subject.fx = null;
+    event.subject.fy = null;
   }
 
 });
