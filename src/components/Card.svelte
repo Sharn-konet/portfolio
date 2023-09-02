a05eb1902ebebe321d63f47069534a960b68c281
Sharn-Konet Reitsma <sharnkonet.reitsma@gmail.com>
2023-09-02 23:02:25 +1200

    Breakout card into its own component

---
+++ src/components/Card.svelte
@@ -0,0 +1,23 @@
+<div id='content'>
+  <slot/>
+</div>
+
+<style>
+
+    #content {
+        background-color: rgb(var(--light-mode-background-color));
+        border-radius: 15px;
+        border-color: rgb(var(--light-mode-text-color));
+        padding: 1em 3em;
+        box-shadow: 0px 0px 60px rgba(var(--light-mode-box-shadow-color), 0.2);
+        transition: box-shadow 3s;
+        margin-top: 3em;
+    }
+
+    :global(body.dark-mode) #content {
+        background-color:   #ffffff20;
+        border-radius: 15px;
+        box-shadow: 0px 0px 80px rgba(var(--light-mode-text-color), 0.20);
+        border-color: rgb(var(--dark-mode-text-color))
+    }
+</style>
--- src/routes/sections/Education.svelte
+++ src/routes/sections/Education.svelte
@@ -1,6 +1,10 @@
+<script lang='ts'>
+  import Card from '@components/Card.svelte'
+</script>
+
 <div id = 'education'>
 <h1>Education</h1>
-<div id = 'content'>
+<Card>
     <h2>Bachelor of Engineering (Honours) in Engineering Science</h2>
     <h3>University of Auckland</h3>
     <p>
@@ -8,29 +12,12 @@
         I primarily explored the theory of data analytics, optimisation, and machine learning, while also dabbling in computational mechanics and software engineering principles.
     </p>
     <p>Graduated with First Class Honours</p>
-</div>
+</Card>
 </div>
 
 <style>
     #education {
         margin: 10% clamp(5%, calc(30% - 100px), 25%);
-    }
-
-    #content {
-        background-color: rgb(var(--light-mode-background-color));
-        border-radius: 15px;
-        border-color: rgb(var(--light-mode-text-color));
-        padding: 1em 3em;
-        box-shadow: 0px 0px 60px rgba(var(--light-mode-box-shadow-color), 0.2);
-        transition: box-shadow 3s;
-        margin-top: 3em;
-    }
-
-    :global(body.dark-mode) #content {
-        background-color:   #ffffff20;
-        border-radius: 15px;
-        box-shadow: 0px 0px 80px rgba(var(--light-mode-text-color), 0.20);
-        border-color: rgb(var(--dark-mode-text-color))
     }
 
     h1 {
