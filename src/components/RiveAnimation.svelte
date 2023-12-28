57270483fb4152b8e30fb27127e09136b56d3368
Sharn-Konet Reitsma <sharnkonet.reitsma@gmail.com>
2023-12-28 13:50:42 +1300

    Fix width bug on phones

--- src/components/RiveAnimation.svelte
+++ src/components/RiveAnimation.svelte
@@ -35,13 +35,13 @@
   .rive-canvas-container {
 		width: 100%;
 		height: 100%;
+    overflow:hidden;
 	}
 
 	#rive-canvas {
 		width: max(100%, 750px);
 		height: 100%;
     margin: -10% 0 -15% 0;
-    overflow: hidden;
     mask: linear-gradient(90deg, transparent, white 20%, white 80%, transparent);
     -webkit-mask: linear-gradient(90deg, transparent, white 20%, white 80%, transparent);
 	}
